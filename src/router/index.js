daec0d417a54777d649365c5ce46d0d14430ea04
Edgar Tolete <57813441+edgartolete@users.noreply.github.com>
2022-05-18 23:38:05 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router' //import the method createRouter and createWebHistory from library vue-router.
+import { createRouter, createWebHistory } from 'vue-router' //import the method createRouter and createWebHistory from library vue-router
 import About from '../views/About.vue' //import the About view
 import Home from '../views/Home.vue' //import the Home view
 
